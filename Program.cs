f5e91604b0d607222c0aa77f86e591147339b530
Vit Bazhin <bazhin@ya.ru>
2022-02-23 17:13:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using ScreenRec;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Start");
+
+new ScreenRecord();
+ 
+
+
+Console.WriteLine("End");
+Console.ReadLine();
---
+++ ScreenRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenRec
+{
+    internal class ScreenRecord
+    {
+        internal ScreenRecord()
+        {
+            SaveVideo();
+        }
+        //Timer timer = new Timer(new TimerCallback(new ));
+        internal void SaveVideo()
+        {
+            Console.WriteLine(GetType().Name);
+            //using (Bitmap bitmap = new(""))
+            //{
+
+            //}
+        }
+    }
+}
